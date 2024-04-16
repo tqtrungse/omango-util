60d187a0ab1285bfee95f2949f0e93f542ca2f13
Trung <tqtrungse@gmail.com>
2024-04-16 14:20:43 +0700

    Fix lint

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ This is an utilities crate.<br />
 - [Backoff](src/backoff.rs): retry with support reduce CPU cache coherence.
 - [CachePadded](src/cache_padded.rs): aligns CPU cache line for types.
 - [SpinLock](src/lock.rs): concurrent thread safe locking for the fast changes.
-- [Any](src/any.rs): wraps `std::any` with useful functions to cast to the original type.
+- [Any](src/any.rs): wraps `std::any::Any` with useful functions to cast to the original type.
 
 ## Table of Contents
 
--- src/any.rs
+++ src/any.rs
@@ -56,43 +56,44 @@ pub trait ANY {
 }
 
 mod test {
-    struct Dummy {}
-    
-    impl Dummy {
-        #[inline(always)]
-        fn new() -> Self {
-            Self{}
-        }
-        
-        fn get_type(&self) -> String {
-            String::from("dummy")
-        }
-    }
-    
-    impl crate::any::ANY for Dummy {
-        #[inline(always)]
-        fn as_mut(&mut self) -> &mut dyn std::any::Any {
-            self
-        }
-    
-        #[inline(always)]
-        fn as_ref(&self) -> &dyn std::any::Any {
-            self
-        }
-    }
-    
-    fn parse(d: &dyn crate::any::ANY) {
-        let r = d.as_ref();
-        if r.type_id() == std::any::TypeId::of::<Dummy>() {
-            assert_eq!(
-                r.downcast_ref::<Dummy>().unwrap().get_type(), 
-                String::from("dummy")
-            );
-        }
-    }
-    
     #[test]
     fn test() {
+        struct Dummy {}
+        
+        impl Dummy {
+            #[inline(always)]
+            fn new() -> Self {
+                Self{}
+            }
+            
+            fn get_type(&self) -> String {
+                String::from("dummy")
+            }
+        }
+        
+        impl crate::any::ANY for Dummy {
+            #[inline(always)]
+            fn as_mut(&mut self) -> &mut dyn std::any::Any {
+                self
+            }
+        
+            #[inline(always)]
+            fn as_ref(&self) -> &dyn std::any::Any {
+                self
+            }
+        }
+        
+        fn parse(d: &dyn crate::any::ANY) {
+            let r = d.as_ref();
+            if r.type_id() == std::any::TypeId::of::<Dummy>() {
+                assert_eq!(
+                    r.downcast_ref::<Dummy>().unwrap().get_type(), 
+                    String::from("dummy")
+                );
+            }
+            assert!(r.downcast_ref::<i32>().is_none());
+        }
+        
         let d = Box::new(Dummy::new());
         parse(d.as_ref());
     }

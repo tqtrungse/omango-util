4f883d8615c0ba9e706b3725d590ac65ba99fbb0
Trung <tqtrungse@gmail.com>
2024-04-17 14:19:54 +0700

    Implement defer

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ This is an utilities crate.<br />
 - [Backoff](src/backoff.rs): retry with support reduce CPU cache coherence.
 - [CachePadded](src/cache_padded.rs): aligns CPU cache line for types.
 - [SpinLock](src/lock.rs): concurrent thread safe locking for the fast changes.
-- [Any](src/any.rs): wraps `std::any::Any` with useful functions to cast to the original type.
+- [Defer](src/defer.rs): support `Golang's defer`.
 
 ## Table of Contents
 
@@ -19,7 +19,7 @@ This is an utilities crate.<br />
 Add this to your `Cargo.toml`:
 ```toml
 [dependencies]
-omango-util = "0.1.2"
+omango-util = "0.1.3"
 ```
 
 ## Compatibility
---
+++ src/defer.rs
@@ -0,0 +1,37 @@
+pub struct Defer<F: FnOnce()> {
+    f: Option<F>,
+}
+
+impl<F: FnOnce()> Defer<F> {
+    #[inline(always)]
+    fn new(f: F) -> Defer<F> {
+        Defer { f: Some(f) }
+    }
+}
+
+impl<F: FnOnce()> Drop for Defer<F> {
+    #[inline(always)]
+    fn drop(&mut self) {
+        if let Some(f) = self.f.take() {
+            f();
+        }
+    }
+}
+
+mod test {
+    #[test]
+    fn test() {
+        fn defer() {
+            let a = 4u32;
+            crate::defer::Defer::new(|| {
+                assert_eq!(1u32, a);
+            });
+        }
+
+        let result = std::panic::catch_unwind(|| {
+            defer();
+        });
+        
+        assert!(result.is_err());
+    }
+}
